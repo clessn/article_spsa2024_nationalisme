186c9422f7e8415d104e69c725e65d3aa0284a8b
hubcad25 <hubertcadieux@gmail.com>
2026-02-01 20:30:49 -0500

    corriger 2021

--- code/census_marginals/fetch_2021.R
+++ code/census_marginals/fetch_2021.R
@@ -1,8 +1,6 @@
 ## cancensus 2021
 ## Fetch census marginals for Quebec using cancensus package
 ## Output: marginals_2021.rds
-##
-## Note: 2021 has language AND education by sex available!
 
 library(cancensus)
 library(dplyr)
@@ -19,51 +17,51 @@ message("Fetching 2021 census data for Quebec...")
 vectors_to_fetch <- c(
   # Age by sex (15+)
   # Male
-  "v_CA21_15",   # 15-19
-  "v_CA21_33",   # 20-24
-  "v_CA21_36",   # 25-29
-  "v_CA21_39",   # 30-34
-  "v_CA21_42",   # 35-39
-  "v_CA21_45",   # 40-44
-  "v_CA21_48",   # 45-49
-  "v_CA21_51",   # 50-54
-  "v_CA21_54",   # 55-59
-  "v_CA21_57",   # 60-64
-  "v_CA21_60",   # 65-69
-  "v_CA21_63",   # 70-74
-  "v_CA21_66",   # 75-79
-  "v_CA21_69",   # 80-84
-  "v_CA21_72",   # 85+
+  "v_CA21_72",   # 15-19
+  "v_CA21_90",   # 20-24
+  "v_CA21_108",  # 25-29
+  "v_CA21_126",  # 30-34
+  "v_CA21_144",  # 35-39
+  "v_CA21_162",  # 40-44
+  "v_CA21_180",  # 45-49
+  "v_CA21_198",  # 50-54
+  "v_CA21_216",  # 55-59
+  "v_CA21_234",  # 60-64
+  "v_CA21_255",  # 65-69
+  "v_CA21_273",  # 70-74
+  "v_CA21_291",  # 75-79
+  "v_CA21_309",  # 80-84
+  "v_CA21_327",  # 85+
   # Female
-  "v_CA21_16",   # 15-19
-  "v_CA21_34",   # 20-24
-  "v_CA21_37",   # 25-29
-  "v_CA21_40",   # 30-34
-  "v_CA21_43",   # 35-39
-  "v_CA21_46",   # 40-44
-  "v_CA21_49",   # 45-49
-  "v_CA21_52",   # 50-54
-  "v_CA21_55",   # 55-59
-  "v_CA21_58",   # 60-64
-  "v_CA21_61",   # 65-69
-  "v_CA21_64",   # 70-74
-  "v_CA21_67",   # 75-79
-  "v_CA21_70",   # 80-84
-  "v_CA21_73",   # 85+
+  "v_CA21_73",   # 15-19
+  "v_CA21_91",   # 20-24
+  "v_CA21_109",  # 25-29
+  "v_CA21_127",  # 30-34
+  "v_CA21_145",  # 35-39
+  "v_CA21_163",  # 40-44
+  "v_CA21_181",  # 45-49
+  "v_CA21_199",  # 50-54
+  "v_CA21_217",  # 55-59
+  "v_CA21_235",  # 60-64
+  "v_CA21_256",  # 65-69
+  "v_CA21_274",  # 70-74
+  "v_CA21_292",  # 75-79
+  "v_CA21_310",  # 80-84
+  "v_CA21_328",  # 85+
 
-  # Language - mother tongue by sex (single responses)
-  "v_CA21_1138",  # English Male
-  "v_CA21_1139",  # English Female
-  "v_CA21_1141",  # French Male
-  "v_CA21_1142",  # French Female
-  "v_CA21_1144",  # Non-official Male
-  "v_CA21_1145",  # Non-official Female
+  # Language - mother tongue by sex
+  "v_CA21_1184",  # English Male
+  "v_CA21_1185",  # English Female
+  "v_CA21_1187",  # French Male
+  "v_CA21_1188",  # French Female
+  "v_CA21_1190",  # Non-official Male
+  "v_CA21_1191",  # Non-official Female
 
-  # Education by sex (15+)
-  "v_CA21_5820",  # Total Male 15+ (for denominator)
-  "v_CA21_5821",  # Total Female 15+ (for denominator)
-  "v_CA21_5850",  # Postsecondary Male
-  "v_CA21_5851"   # Postsecondary Female
+  # Education by sex
+  "v_CA21_5818",  # Total Male 15+
+  "v_CA21_5819",  # Total Female 15+
+  "v_CA21_5827",  # Postsecondary Male
+  "v_CA21_5828"   # Postsecondary Female
 )
 
 qc_data <- get_census(
@@ -91,13 +89,13 @@ age_male <- tibble(
   category = c("15-24", "25-34", "35-44", "45-54", "55-64", "65+"),
   gender = "male",
   n = c(
-    get_val("v_CA21_15") + get_val("v_CA21_33"),
-    get_val("v_CA21_36") + get_val("v_CA21_39"),
-    get_val("v_CA21_42") + get_val("v_CA21_45"),
-    get_val("v_CA21_48") + get_val("v_CA21_51"),
-    get_val("v_CA21_54") + get_val("v_CA21_57"),
-    get_val("v_CA21_60") + get_val("v_CA21_63") + get_val("v_CA21_66") +
-      get_val("v_CA21_69") + get_val("v_CA21_72")
+    get_val("v_CA21_72") + get_val("v_CA21_90"),
+    get_val("v_CA21_108") + get_val("v_CA21_126"),
+    get_val("v_CA21_144") + get_val("v_CA21_162"),
+    get_val("v_CA21_180") + get_val("v_CA21_198"),
+    get_val("v_CA21_216") + get_val("v_CA21_234"),
+    get_val("v_CA21_255") + get_val("v_CA21_273") + get_val("v_CA21_291") +
+      get_val("v_CA21_309") + get_val("v_CA21_327")
   )
 )
 
@@ -106,27 +104,27 @@ age_female <- tibble(
   category = c("15-24", "25-34", "35-44", "45-54", "55-64", "65+"),
   gender = "female",
   n = c(
-    get_val("v_CA21_16") + get_val("v_CA21_34"),
-    get_val("v_CA21_37") + get_val("v_CA21_40"),
-    get_val("v_CA21_43") + get_val("v_CA21_46"),
-    get_val("v_CA21_49") + get_val("v_CA21_52"),
-    get_val("v_CA21_55") + get_val("v_CA21_58"),
-    get_val("v_CA21_61") + get_val("v_CA21_64") + get_val("v_CA21_67") +
-      get_val("v_CA21_70") + get_val("v_CA21_73")
+    get_val("v_CA21_73") + get_val("v_CA21_91"),
+    get_val("v_CA21_109") + get_val("v_CA21_127"),
+    get_val("v_CA21_145") + get_val("v_CA21_163"),
+    get_val("v_CA21_181") + get_val("v_CA21_199"),
+    get_val("v_CA21_217") + get_val("v_CA21_235"),
+    get_val("v_CA21_256") + get_val("v_CA21_274") + get_val("v_CA21_292") +
+      get_val("v_CA21_310") + get_val("v_CA21_328")
   )
 )
 
-# --- LANGUAGE MARGINALS (by sex!) ---
-message("Creating language marginals (by sex)...")
+# --- LANGUAGE MARGINALS (by sex) ---
+message("Creating language marginals...")
 
 lang_male <- tibble(
   variable = "language",
   category = c("english", "french", "other"),
   gender = "male",
   n = c(
-    get_val("v_CA21_1138"),
-    get_val("v_CA21_1141"),
-    get_val("v_CA21_1144")
+    get_val("v_CA21_1184"),
+    get_val("v_CA21_1187"),
+    get_val("v_CA21_1190")
   )
 )
 
@@ -135,19 +133,19 @@ lang_female <- tibble(
   category = c("english", "french", "other"),
   gender = "female",
   n = c(
-    get_val("v_CA21_1139"),
-    get_val("v_CA21_1142"),
-    get_val("v_CA21_1145")
+    get_val("v_CA21_1185"),
+    get_val("v_CA21_1188"),
+    get_val("v_CA21_1191")
   )
 )
 
 # --- EDUCATION MARGINALS (by sex) ---
 message("Creating education marginals...")
 
-male_total <- get_val("v_CA21_5820")
-female_total <- get_val("v_CA21_5821")
-male_postsec <- get_val("v_CA21_5850")
-female_postsec <- get_val("v_CA21_5851")
+male_total <- get_val("v_CA21_5818")
+female_total <- get_val("v_CA21_5819")
+male_postsec <- get_val("v_CA21_5827")
+female_postsec <- get_val("v_CA21_5828")
 
 edu_male <- tibble(
   variable = "education",
